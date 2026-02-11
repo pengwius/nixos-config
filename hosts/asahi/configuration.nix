efabf426838ed790e695969b478a74a74865f79b
pengwius <pengwius@protonmail.ch>
2026-02-11 17:00:32 +0100

    switch mesa to nixpkgs

--- hosts/asahi/configuration.nix
+++ hosts/asahi/configuration.nix
@@ -36,7 +36,7 @@
   };
 
   boot.binfmt.emulatedSystems = [
-   "x86_64-linux"
+    "x86_64-linux"
   ];
 
   nixpkgs.config.allowUnsupportedSystem = true;
@@ -60,13 +60,8 @@
   };
 
   hardware.graphics.package =
-    # FIXME: Workaround for Mesa 25.3.0 regression
-    # https://github.com/nix-community/nixos-apple-silicon/issues/380
     assert pkgs.mesa.version == "25.3.4";
-    (import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/c5ae371f1a6a7fd27823bc500d9390b38c05fa55.tar.gz";
-      sha256 = "sha256-4PqRErxfe+2toFJFgcRKZ0UI9NSIOJa+7RXVtBhy4KE=";
-    }) { localSystem = pkgs.stdenv.hostPlatform; }).mesa;
+    pkgs.mesa;
 
   powerManagement = {
     enable = true;
